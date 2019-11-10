a9e7c80bbac98a6e9a3a8d21fc704274d46dffe1
munzir1996 <munzirmukhtar996@gmail.com>
2019-11-10 09:06:02 +0200

    3. Create Task

--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -22,6 +22,19 @@ contract('TodoList', (accounts) => {
         assert.equal(task.completed, false)
         assert.equal(taskCount.toNumber(), 1)
     })
+
+    it('Task Created', async() => {
+        const result = await this.todoList.createTask('Create Task')
+        const taskCount = await this.todoList.taskCount()
+
+        assert.equal(taskCount, 2)
+
+        const event = result.logs[0].args
+        assert.equal(event.id.toNumber(), 2)
+        assert.equal(event.content, 'Create Task')
+        assert.equal(event.completed, false)
+
+    })
 })
 
 
